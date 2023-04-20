26f77fd42c2f79bb496d9949bce80293eed028d0
ServandoL7 <selara@ucsd.edu>
2023-04-20 09:10:48 -0700

    Array Method Fixes

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,23 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
-
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverseInPLace3() {
+    int[] input = { 2, 4, 6 };
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[]{ 6, 4, 2 }, input);
+  }
+
+  @Test
+  public void testReversedsize3() {
+    int[] input1 = { 3, 5, 7 };
+    ArrayExamples.reversed(input1);
+    assertArrayEquals(new int[]{ 7, 5, 3 }, ArrayExamples.reversed(input1));
   }
 }
